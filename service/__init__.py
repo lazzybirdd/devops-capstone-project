6645b974b25413e0d992f47d77d276f95e8c8392
lazzybirdd <rbarinov2004@yahoo.com>
2023-11-08 18:38:49 -0800

    Issue 14 - added CORS (#17)
    * Added security headers
    
    * Added CORS headers

--- service/__init__.py
+++ service/__init__.py
@@ -8,7 +8,10 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+
+# RB:
 from flask_talisman import Talisman
+from flask_cors import CORS
 
 # Create Flask application
 app = Flask(__name__)
@@ -16,6 +19,8 @@ app.config.from_object(config)
 
 # initialize Flask-Talisman
 talisman = Talisman(app)
+# initialize CORS
+CORS(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
